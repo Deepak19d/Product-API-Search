689f21ac542fb4cb768f280b89ee80c673cda2e9
Deepak kumar <deepak19122002@gmail.com>
2024-11-14 01:33:30 +0530

    commit

--- index.js
+++ index.js
@@ -24,24 +24,30 @@ function displayproductData(productTodisplay)
         let sntd = document.createElement("td");
         let titletd = document.createElement("td");
         let brandtd = document.createElement("td");
-        let imgtd = document.createElement("td");
+        let imgtd = document.createElement("img");
         let stocktd = document.createElement("td");
         let descriptiontd = document.createElement("td");
         let weighttd = document.createElement("td");
+        let imgtdAppend = document.createElement("td");
+
 
         sntd.textContent = index +1
         titletd.textContent =res.title
         brandtd.textContent = res.brand
-        imgtd.textContent=res.productImg
+        imgtd.src=res.thumbnail
+
+        imgtd.style.width = "50px"
+
         stocktd.textContent = res.stock
         descriptiontd.textContent = res.description
         weighttd.textContent = res.weight
         //append data
 
+        imgtdAppend.appendChild(imgtd)
         tr.appendChild(sntd)
         tr.appendChild(titletd)
         tr.appendChild(brandtd)
-        tr.appendChild(imgtd)
+        tr.appendChild(imgtdAppend)
         tr.appendChild(stocktd)
         tr.appendChild(descriptiontd)
         tr.appendChild(weighttd)
